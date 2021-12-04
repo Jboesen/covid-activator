26b83b13c64b7bbfb23f3d21a168bc1fd006a57e
Jboesen <89944638+Jboesen@users.noreply.github.com>
2021-12-04 15:08:32 -0500

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -5,7 +5,6 @@ from functools import wraps
 import os
 import pytesseract
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
 try:
     from PIL import Image
 except ImportError:
@@ -74,46 +73,41 @@ def get_pw(enc_cpw):
 
 
 def activate_test(email, decrypted, barcode, acc_num):
-    try:
-        driver = webdriver.Chrome("/usr/bin/chromedriver")
-        print("Successfully set Chrome to webdriver...")
-        driver.get("https://home.color.com/sign-in?next=%2Fcovid%2Factivation")
-        driver.find_element_by_name("email").send_keys(email)
-        driver.find_element_by_name("password").send_keys(decrypted)
-        # can I just plug in all these class elements?
-        login_bt_cls = "MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-containedSizeLarge MuiButton-sizeLarge MuiButton-fullWidth"
-        driver.find_element_by_class_name(login_bt_cls).click()
-        person_bt_cls = "MuiButtonBase-root MuiButton-root MuiButton-outlined jss268 MuiButton-outlinedPrimary"
-        driver.find_element_by_class_name(person_bt_cls).click()
-        print("Successfully logged in...")
+    driver = webdriver.Chrome("/usr/bin/chromedriver")
+    driver.get("https://home.color.com/sign-in?next=%2Fcovid%2Factivation")
+    driver.find_element_by_name("email").send_keys(email)
+    driver.find_element_by_name("password").send_keys(decrypted)
+    # can I just plug in all these class elements?
+    login_bt_cls = "MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-containedSizeLarge MuiButton-sizeLarge MuiButton-fullWidth"
+    driver.find_element_by_class_name(login_bt_cls).click()
+    person_bt_cls = "MuiButtonBase-root MuiButton-root MuiButton-outlined jss268 MuiButton-outlinedPrimary"
+    driver.find_element_by_class_name(person_bt_cls).click()
 
-        start_bt_cls = "MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary Link_Link__2jUr8"
-        driver.find_element_by_class_name(start_bt_cls).click()
+    start_bt_cls = "MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary Link_Link__2jUr8"
+    driver.find_element_by_class_name(start_bt_cls).click()
 
-        no_bt_cls = "QuizChoice tl w-100 flex justify-between bg-white pointer hover-opacity-0-75"
-        driver.find_element_by_class_name(no_bt_cls).click()
+    no_bt_cls = "QuizChoice tl w-100 flex justify-between bg-white pointer hover-opacity-0-75"
+    driver.find_element_by_class_name(no_bt_cls).click()
 
-        cont_bt_cls = "MuiButtonBase-root MuiButton-root MuiButton-contained ActionButtons_NextButton__MEOPx MuiButton-containedPrimary MuiButton-containedSizeLarge MuiButton-sizeLarge"
-        driver.find_element_by_class_name(cont_bt_cls).click()
+    cont_bt_cls = "MuiButtonBase-root MuiButton-root MuiButton-contained ActionButtons_NextButton__MEOPx MuiButton-containedPrimary MuiButton-containedSizeLarge MuiButton-sizeLarge"
+    driver.find_element_by_class_name(cont_bt_cls).click()
 
-        driver.find_element_by_name("primaryConsentIsAccepted").click()
-        driver.find_element_by_name("additionalConsents[0]").click()
-        driver.find_element_by_name("additionalConsents[1]").click()
-        driver.find_element_by_name("additionalConsents[2]").click()
+    driver.find_element_by_name("primaryConsentIsAccepted").click()
+    driver.find_element_by_name("additionalConsents[0]").click()
+    driver.find_element_by_name("additionalConsents[1]").click()
+    driver.find_element_by_name("additionalConsents[2]").click()
 
-        cont_bt_cls2 = "MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-containedSizeLarge MuiButton-sizeLarge"
-        driver.find_element_by_class_name(cont_bt_cls2).click()
+    cont_bt_cls2 = "MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary MuiButton-containedSizeLarge MuiButton-sizeLarge"
+    driver.find_element_by_class_name(cont_bt_cls2).click()
 
-        driver.find_element_by_class_name(cont_bt_cls2).click()
-        conf_cont_bt_cls = "MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary"
-        driver.find_element_by_class_name(conf_cont_bt_cls).click()
+    driver.find_element_by_class_name(cont_bt_cls2).click()
+    conf_cont_bt_cls = "MuiButtonBase-root MuiButton-root MuiButton-contained MuiButton-containedPrimary"
+    driver.find_element_by_class_name(conf_cont_bt_cls).click()
 
-        driver.find_element_by_name("kit_barcode").send_keys(barcode)
-        driver.find_element_by_name("accession_number").send_keys(acc_num)
-        driver.find_element_by_class_name(cont_bt_cls2).click()
-        driver.find_element_by_class_name(conf_cont_bt_cls).click()
-        if driver.find_element_by_class_name("MuiTypography-root jss2 MuiTypography-h1").text == "You’ve activated your kit! Now, collect a sample.":
-            return True
-    except NoSuchElementException:
-        return False
+    driver.find_element_by_name("kit_barcode").send_keys(barcode)
+    driver.find_element_by_name("accession_number").send_keys(acc_num)
+    driver.find_element_by_class_name(cont_bt_cls2).click()
+    driver.find_element_by_class_name(conf_cont_bt_cls).click()
+    if driver.find_element_by_class_name("MuiTypography-root jss2 MuiTypography-h1").text == "You’ve activated your kit! Now, collect a sample.":
+        return True
     return False
